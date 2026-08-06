de0439777d22433f128de85ba7021d067c5940de
hallaneduardosilva-code <hallan.eduardo.silva@escola.pr.gov.br>
2026-08-06 10:43:12 -0300

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+//botões
+
+const btnContraste = document.getElementById("btn-contraste");
+const btnAumentar = document.getElementById("btn-aumentar-texto");
+const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+//Mudar tamanho a fonte
+
+let tamanhoAtualFonte = 100;
+
+//Alto constraste
+
+btnContraste.addEventListener("click")
